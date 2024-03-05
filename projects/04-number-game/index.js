ade0ceac482b622bcf36bb92d7e80b8abd6dda97
Delucx <juliusmiguelsrayray@iskolarngbayan.pup.edu.ph>
2024-03-05 12:07:06 +0800

    new

--- projects/04-number-game/index.js
+++ projects/04-number-game/index.js
@@ -1,7 +1,12 @@
 const guessNumber = document.getElementById('guessNumber');
 const message = document.getElementById('message');
 const checkNumber = document.getElementById('checkNumber');
+const tryAgain = document.getElementById('tryAgain');
 const reset = document.getElementById('resetGame');
+const guessedNumber = document.getElementById('guessedNumber');
+const guessesCount = document.getElementById('guessesCount');
+guessedNumber.textContent = 0;
+guessesCount.textContent = 0;
 
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
@@ -17,12 +22,34 @@ checkNumber.addEventListener('click', function () {
         const number = parseInt(guessNumber.value);
         if (number === randomNumber) {
             message.textContent = 'Congratulations! You guessed the number!';
+            message.style.color = 'green';
+            checkNumber.disabled = true;
+            guessNumber.disabled = true;
+            guessedNumber.textContent = parseInt(guessedNumber.textContent) + 1;
         } else if (number > randomNumber) {
             message.textContent = 'Too high! Try again!';
+            message.style.color = 'red';
+            guessesCount.textContent = parseInt(guessesCount.textContent) + 1;
         } else {
             message.textContent = 'Too low! Try again!';
+            message.style.color = 'red';
+            guessesCount.textContent = parseInt(guessesCount.textContent) + 1;
         }
 
+    }
+});
+
+
+
+tryAgain.addEventListener('click', function () {
+    if (tryAgain) {
+        let random = generateRandomNumber();
+        randomNumber = random;
+        console.log(randomNumber);
+        guessNumber.value = '';
+        message.textContent = '';
+        checkNumber.disabled = false;
+        guessNumber.disabled = false;
     }
 });
 
@@ -33,6 +60,10 @@ reset.addEventListener('click', function () {
         console.log(randomNumber);
         guessNumber.value = '';
         message.textContent = '';
+        guessesCount.textContent = 0;
+        guessedNumber.textContent = 0;
+        checkNumber.disabled = false;
+        guessNumber.disabled = false;
     }
 });
 
